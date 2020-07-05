5f0daaf613321c991b38c728e36004d0de23596f
ntrix <ntrix3390@gmail.com>
2020-07-05 22:39:53 +0200

    multi classes binding home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,4 +16,12 @@ export class HomeComponent implements OnInit {
   countClick() {
     this.clickCounter += 1;
   }
+
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4,
+    };
+    return myClasses;
+  }
 }
