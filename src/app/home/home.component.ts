3df13eecf5fba9a0b24cddba11e689ac5b830c0e
ntrix <ntrix3390@gmail.com>
2020-07-05 22:06:50 +0200

    create home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  clickCounter: number = 0;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  countClick() {
+    this.clickCounter += 1;
+  }
+}
