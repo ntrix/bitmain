0ed7e17482b262a22387c009dcd7a045df64885f
ntrix <ntrix3390@gmail.com>
2020-07-06 00:07:06 +0200

    http service test method

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  constructor() {}
 
-  constructor() { }
+  myMethod() {
+    return console.log('Hey, what is up!');
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,15 @@
+import { HttpService } from './../http.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor(private _http: HttpService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this._http.myMethod();
   }
-
 }
