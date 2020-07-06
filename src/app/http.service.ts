f4e7e8c069be0dacd4fdd5a30fa81b41bf87a055
ntrix <ntrix3390@gmail.com>
2020-07-06 20:20:06 +0200

    http service api of bitfinex and proxy for CORS

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -5,9 +5,12 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class HttpService {
+  bfxURL: string = 'https://api-pub.bitfinex.com/v2/tickers?symbols=ALL';
+  proxyURL: string = 'https://cors-anywhere.herokuapp.com/';
+
   constructor(private http: HttpClient) {}
 
   getBeer() {
-    return this.http.get('https://api.openbrewerydb.org/breweries');
+    return this.http.get(this.proxyURL + this.bfxURL);
   }
 }
