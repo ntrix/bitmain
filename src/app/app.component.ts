57776071dc46fb5e5c7d284cbfff3f330c9f76cc
ntrix <ntrix3390@gmail.com>
2020-07-07 20:30:00 +0200

    temporary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Weenie';
+
+  darkModeActive: boolean = true;
+  showMenu: boolean = false;
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch() {
+    this.darkModeActive = !this.darkModeActive;
+  }
 }
