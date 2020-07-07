70c813c3fefb0ea6e207c63ea1a6d8d85b020c31
ntrix <ntrix3390@gmail.com>
2020-07-07 22:51:16 +0200

    style: color patern definition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Weenie';
+  title = 'Bitmain';
 
   darkModeActive: boolean = true;
   showMenu: boolean = false;
