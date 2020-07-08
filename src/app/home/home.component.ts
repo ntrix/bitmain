c3acbab47e83de75ffbfb9246c6d33859b24af3e
ntrix <ntrix3390@gmail.com>
2020-07-08 13:27:13 +0200

    first step forex component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,19 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  clickCounter: number = 0;
-  name: string = '';
-
   constructor() {}
 
   ngOnInit() {}
-
-  // countClick() {
-  //   this.clickCounter += 1;
-  // }
-
-  // setClasses = () =>
-  //   this.clickCounter <= 4
-  //     ? { active: false, notactive: true }
-  //     : { active: true, notactive: false };
 }
--- src/app/trade/trade.component.ts
+++ src/app/trade/trade.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-trade',
   templateUrl: './trade.component.html',
-  styleUrls: ['./trade.component.scss']
+  styleUrls: ['./trade.component.scss'],
 })
 export class TradeComponent implements OnInit {
+  clickCounter: number = 0;
+  name: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  countClick() {
+    this.clickCounter += 1;
   }
 
+  setClasses = () =>
+    this.clickCounter <= 4
+      ? { active: false, notactive: true }
+      : { active: true, notactive: false };
 }
