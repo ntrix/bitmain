c782d648f0f1c3be4e3688dea68a213bd1ba942f
ntrix <ntrix3390@gmail.com>
2020-07-08 14:51:04 +0200

    create fx http services

---
+++ src/app/fx-services.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FxServicesService {
+  fxURL: string =
+    'https://www.freeforexapi.com/api/live?pairs=EURUSD,EURGBP,USDJPY';
+  proxyURL: string = '';
+  // 'https://cors-anywhere.herokuapp.com/';
+
+  constructor(private http: HttpClient) {}
+
+  getBeer() {
+    return this.http.get(this.proxyURL + this.fxURL);
+  }
+}
