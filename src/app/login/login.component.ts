6b847efc792a8fb10f0453f7d40ab8eebb8bc998
ntrix <ntrix3390@gmail.com>
2020-07-08 16:58:03 +0200

    add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  errorMessage = '';
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  login(e) {
+    // this.fb.signin(e.target.email.value, e.target.password.value).pipe(first()).subscribe(() => {
+    //   this.router.navigateByUrl('');
+    // },(err) => {
+    //   this.errorMessage = err;
+    //   setTimeout(() => this.errorMessage = '', 2000);
+    // });
   }
-
 }
